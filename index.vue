67195758c5ea3e67177cf069eab2f9765ba54d00
yejiaming <2508548688@qq.com>
2017-10-27 11:29:57 +0800

    模拟textarea解决高度自适应问题

--- index.vue
+++ index.vue
@@ -1,60 +1,16 @@
-# 使用contenteditable+div模拟textarea文本域实现高度自适应
-开发过程中由于需要在发送消息的时候需要有一个可以高度自适应的文本域，一开始是使用textarea并搭配auto-size插件来做到textarea的高度自适应，后来因为遇到一些问题，而且也多加了依赖缺乏可定制，所以决定使用contenteditable来实现。
-
-## contenteditable介绍
-
-**contenteditable**属性规定元素内容是否可编辑，是H5新增的属性，支持情况相当好，基本上所有的浏览器都兼容。
-
-**语法：**
-
-```html
-<element contenteditable="true|false">
-```
-## 实现主要代码如下
-代码实现是基于vue来实现的。
-**html部分：**
-
-```html
 <template>
     <div class="textarea" contenteditable="true"></div>
 </template>
-```
-**CSS部分**
-
-```css
-<style scoped lang="less" rel="stylesheet/less">
-    .textarea {
-        box-sizing: border-box;
-        min-height: 136px;
-        max-height: 300px;
-        margin-left: auto;
-        margin-right: auto;
-        padding: 3px;
-        outline: 0;
-        border: 1px solid #a0b3d6;
-        font-size: 12px;
-        word-wrap: break-word;
-        overflow-x: hidden;
-        overflow-y: auto;
-        _overflow-y: visible;
-        -webkit-user-modify: read-write-plaintext-only; // 只是编辑text文本，只能解决webkit内核里面问题，手机端适用
-        -webkit-user-select: text; // 解决IOS部分手机不支持contenteditable=true属性问题
-        p {
-            margin: 0;
-        }
-    }
-</style>
-```
-**代码解读：**
-
-* 设置-webkit-user-modify属性，是为了在剪切复制的时候会把剪切的内容的格式也一并带过来，由于我们是仿写textarea，是不支持富文本的，所以需要需要将内容格式化成文本格式，而该属性在webkit内核下就可以达到我们的目的。
-* 设置-webkit-user-select属性，是为了解决在测试过程中出现部分IOS手机不支持contenteditable属性的问题。
-
-**JS部分：**
-
-```js
+<!--使用该组件注意一个问题就是不要在可视化区域的节点上使用-webkit-user-select: none;样式，否则会出现当鼠标焦点小时光标和小水滴无法消失的情况-->
 <script type="text/babel">
     export default {
+        data () {
+            return {}
+        },
+        computed: {},
+        props: {},
+        beforeMount () {
+        },
         mounted() {
             this.addInputEvent();
             this.addFocusEvent();
@@ -184,20 +140,28 @@
             }
         },
     }
+
 </script>
-```
-**代码解读：**
 
-* 其中**addEventPaste**方法是为了解决非webkit内核对于**-webkit-user-select**属性支持不好的问题。里面主要是监听黏贴事件然后或者剪切板的文本内容然后再阻止黏贴事件，并将文本内容追加到光标中，并将光标移动到相应的位置。
-* 其中**replaceToBreak**方法是为了解决在textarea中换行的问题，在该伪textarea中换行是会单独将换行内容放到新的DIV中的，所以，当我们需要对该内容进行格式化处理才行。
-* **this.$el.scrollIntoView**的作用是为了当使用者将我们的输入框是使用绝对定位放在页面底部的时候而被虚拟键盘遮挡的问题。
-
-## issue
-* 使用该组件注意一个问题就是不要在可视化区域的节点上使用-**webkit-user-select: none**样式，否则会出现当鼠标焦点小时光标和小水滴无法消失的情况
-
-## 参考链接
-
-[div模拟textarea文本域轻松实现高度自适应](http://www.zhangxinxu.com/wordpress/2010/12/div-textarea-height-auto/)
-[如何让contenteditable元素只能输入纯文本](http://www.zhangxinxu.com/wordpress/2016/01/contenteditable-plaintext-only/)
-
-
+<style scoped lang="less" rel="stylesheet/less">
+    .textarea {
+        box-sizing: border-box;
+        min-height: 136px;
+        max-height: 300px;
+        margin-left: auto;
+        margin-right: auto;
+        padding: 3px;
+        outline: 0;
+        border: 1px solid #a0b3d6;
+        font-size: 12px;
+        word-wrap: break-word;
+        overflow-x: hidden;
+        overflow-y: auto;
+        _overflow-y: visible;
+        -webkit-user-modify: read-write-plaintext-only; // 只是编辑text文本，只能解决webkit内核里面问题，手机端适用
+        -webkit-user-select: text; // 解决IOS部分手机不支持contenteditable=true属性问题
+        p {
+            margin: 0;
+        }
+    }
+</style>
